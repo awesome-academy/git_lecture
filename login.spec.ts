8c4bdaa760cfd128a48a248e1c3f202d5b3192de
vanvtt-0952 <vu.thi.tran.van@sun-asterisk.com>
2025-10-03 11:38:54 +0700

    Commit of branch demo 1

--- login.spec.ts
+++ login.spec.ts
@@ -12,19 +12,21 @@ test.describe('Login Page Tests', () => {
         await expect(page.locator('.inventory_list')).toBeVisible();
     });
 
-    test('should show error with invalid password', async ({ page }) => {
+
+    test('Login fails with invalid password', async ({ page }) => {
         await page.goto(baseURL);
-        await page.fill('#user-name', 'standard_user');
-        await page.fill('#password', 'wrong_password');
-        await page.click('#login-button');
+        await page.fill('[data-test="username"]', 'standard_user');
+        await page.fill('[data-test="password"]', 'wrong_password');
+        await page.click('[data-test="login-button"]');
         await expect(page.locator('[data-test="error"]')).toHaveText(/Username and password do not match/);
     });
 
-    test('should show error when username is empty', async ({ page }) => {
+
+    test('Login fails with locked out user', async ({ page }) => {
         await page.goto(baseURL);
-        await page.fill('#user-name', '');
-        await page.fill('#password', 'secret_sauce');
-        await page.click('#login-button');
-        await expect(page.locator('[data-test="error"]')).toHaveText(/Username is required/);
+        await page.fill('[data-test="username"]', 'locked_out_user');
+        await page.fill('[data-test="password"]', 'secret_sauce');
+        await page.click('[data-test="login-button"]');
+        await expect(page.locator('[data-test="error"]')).toHaveText(/Sorry, this user has been locked out/);
     });
 });
